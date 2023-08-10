41bb3004fed20fa652038067415e217b86aafada
jitender <113350806+jitenderji1137@users.noreply.github.com>
2023-08-11 01:18:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  distDir: '_static',
+  distDir: 'build',
   images: {
     unoptimized: true,
     domains: ['i.postimg.cc',"i.ibb.co","i.ytimg.com","filmdaily.co","occ-0-4773-2164.1.nflxso.net","occ-0-6215-2164.1.nflxso.net"],
