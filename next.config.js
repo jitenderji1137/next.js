ceaf9a1c92b5b4882b995cb57e0782195ac4f475
jitender <trademetrader1137@gmail.com>
2023-08-11 01:05:46 +0530

    Free Netflix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: '_static',
   images: {
+    unoptimized: true,
     domains: ['i.postimg.cc',"i.ibb.co","i.ytimg.com","filmdaily.co","occ-0-4773-2164.1.nflxso.net","occ-0-6215-2164.1.nflxso.net"],
   },
 };
