5c30bfa3d7887aa1f2d3028837f42efb408fa4cc
jitender <trademetrader1137@gmail.com>
2023-08-16 14:20:08 +0530

    free-netflix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,11 +8,11 @@ export default function App({ Component, pageProps }) {
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Piedra&display=swap"/>
           <link rel="icon" href="/logo.png" />
-          <Script type='text/javascript' src='//toothbrushlimbperformance.com/e3/84/8c/e3848cc2ba52f8c8c8a0c97335996bea.js'/>
+          <script type='text/javascript' src='//toothbrushlimbperformance.com/e3/84/8c/e3848cc2ba52f8c8c8a0c97335996bea.js'/>
         </Head>
         <Navbar/>
         <Component {...pageProps} />
-        <Script type='text/javascript' src='//toothbrushlimbperformance.com/8c/89/a3/8c89a37d7a271d17f9442787e948475f.js'/>
+        <Script type='text/javascript' src='//toothbrushlimbperformance.com/8c/89/a3/8c89a37d7a271d17f9442787e948475f.js'></Script>
     </>
   )
 }
