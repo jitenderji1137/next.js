11df65f1b776e7c254dd421981b1cd253c2a65b1
jitender <trademetrader1137@gmail.com>
2023-08-16 13:59:58 +0530

    free-netflix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
+import Script from 'next/script'
 import Navbar from '@/components/navbar/navbar'
 export default function App({ Component, pageProps }) {
   return (
@@ -7,9 +8,11 @@ export default function App({ Component, pageProps }) {
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Piedra&display=swap"/>
           <link rel="icon" href="/logo.png" />
+          <Script type='text/javascript' src='//toothbrushlimbperformance.com/e3/84/8c/e3848cc2ba52f8c8c8a0c97335996bea.js'/>
         </Head>
         <Navbar/>
         <Component {...pageProps} />
+        <Script type='text/javascript' src='//toothbrushlimbperformance.com/8c/89/a3/8c89a37d7a271d17f9442787e948475f.js'/>
     </>
   )
 }
