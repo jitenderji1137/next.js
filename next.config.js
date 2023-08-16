c88bc8592d65bb0a2c5d380360c97fb132efa761
jitender <trademetrader1137@gmail.com>
2023-08-16 14:55:22 +0530

    free-netflix

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
     unoptimized : true,
     domains: ['i.postimg.cc',"i.ibb.co","i.ytimg.com","filmdaily.co","occ-0-4773-2164.1.nflxso.net","occ-0-6215-2164.1.nflxso.net","media.thekashmirmonitor.net","vijaysolution.com","occ-0-64-1348.1.nflxso.net","webseriestale.com","britasia.tv","www.iwmbuzz.com","www.famousbollywood.com","img1.hotstarext.com","images.ottplay.com","www.deccanherald.com","stat4.bollywoodhungama.in","imgnew.outlookindia.com","img.doodcdn.co","www.91-cdn.com","shoutingstars.com","occ-0-1433-1723.1.nflxso.net","ullu2-files.ullu.app"],
   },
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  }
 };
 
 module.exports = nextConfig
