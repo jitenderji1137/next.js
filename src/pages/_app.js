6e30579f04b76287ab69c3c25764180f7704e156
jitender <113350806+jitenderji1137@users.noreply.github.com>
2023-08-22 21:50:28 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import Head from 'next/head'
-import Script from 'next/script'
 import Navbar from '@/components/navbar/navbar'
 import Footer from '@/components/footer/footer'
 export default function App({ Component, pageProps }) {
@@ -10,12 +9,10 @@ export default function App({ Component, pageProps }) {
           <Head>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Piedra&display=swap"/>
             <link rel="icon" href="/logo.png" />
-            // <script type='text/javascript' src='//toothbrushlimbperformance.com/e3/84/8c/e3848cc2ba52f8c8c8a0c97335996bea.js'/>
           </Head>
           <Navbar/>
           <Component {...pageProps} />
           <Footer/>
-          // <Script type='text/javascript' src='//toothbrushlimbperformance.com/8c/89/a3/8c89a37d7a271d17f9442787e948475f.js'></Script>
       </html>
     </>
   )
