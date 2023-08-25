05784401cf9fd0bb6b3b9c7dd0279f108ba7a31b
jitenderji1137 <trademetrader1137@gmail.com>
2023-08-25 22:06:44 +0530

    Free Netflix

--- src/components/slider/emptyslider.jsx
+++ src/components/slider/emptyslider.jsx
@@ -10,7 +10,7 @@ export default function EmptySlider({text}) {
                 <div className="flex flex-nowrap overflow-x-scroll overflow-y-hidden scroll-smooth w-screen px-5 x-scrollbar">
                     {[...Array(10)].map((_, index) => (
                         <div key={index} className="aspect-video h-40 p-1 rounded mt-5 mb-5">
-                            <Image className='rounded h-full cursor-pointer transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-125 hover:bg-indigo-500 duration-300' priority={true} src={process.env.NEXT_PUBLIC_LOADING_IMAGE} alt={`Loading...`} width={1000} height={1000} />
+                            <Image className='rounded h-full cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:bg-indigo-500 duration-300' priority={true} src={process.env.NEXT_PUBLIC_LOADING_IMAGE} alt={`Loading...`} width={1000} height={1000} />
                         </div>
                     ))}
                 </div>
