3502f5fdf770b9e79bc1c9c9dce1a15d083eb98f
jitenderji1137 <trademetrader1137@gmail.com>
2023-08-26 12:42:23 +0530

    Free Netflix

--- src/pages/player/[...player].jsx
+++ src/pages/player/[...player].jsx
@@ -1,98 +1,30 @@
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import Script from 'next/script';
 import { createClient } from '@supabase/supabase-js';
-import { Make_A_Fetch_Request } from '@/components/request/makerequest';
-import ViewAllRender from '@/components/viewall/viewall';
-import EmptyViewAllRender from '@/components/viewall/emptyviewall';
-
-export default function Player() {
-  const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
-  const router = useRouter();
-  const { player } = router.query;
-  const [iframe, setIframe] = useState(null);
-  const [video, setVideo] = useState({});
-  const [image, setImage] = useState('');
-  const [pagedata, setPagedata] = useState([]);
-  const [start, setStart] = useState(0);
-  const [end, setEnd] = useState(49);
-
-  const fetchBanner = async () => {
-    try {
-      const response = await supabase
-        .from('Free-Netflix-Darabase')
-        .select('*')
-        .eq('ID', player[0]);
-
-      if (response.data && response.data.length > 0) {
-        const videoData = response.data[0];
-        setVideo(videoData);
-        setImage(videoData.Image);
-
-        let iframeUrl = '';
-        if (videoData.Plateform === 'filemoon') {
-          iframeUrl = `https://filemoon.sx/e/${videoData.FileID}?poster=${videoData.Image}`;
-        } else if (videoData.Plateform === 'Youtube') {
-          iframeUrl = `https://www.youtube.com/embed/${videoData.FileID}`;
-        } else if (videoData.Plateform === 'Vidsrc') {
-          const embedType = videoData.MainCategory === 'TV' ? 'tv' : 'movie';
-          iframeUrl = `https://vidsrc.to/embed/${embedType}/${videoData.FileID}`;
-        } else if (videoData.Plateform === 'vidsrc') {
-          const embedType = videoData.MainCategory === 'TV' ? 'tv' : 'movie';
-          iframeUrl = `https://vidsrc.me//embed//${embedType}?imdb=${videoData.FileID}`;
-        }
-
-        setIframe(iframeUrl);
-
-        const category = videoData.MainCategory;
-        const fetchedData = await Make_A_Fetch_Request('MainCategory', [category], start, end);
-        setPagedata(fetchedData);
-      }
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-  useEffect(() => {
-    if (player !== undefined) {
-      const playerPage = parseInt(player[1], 10);
-      const newStart = (playerPage - 1) * 49;
-      const newEnd = playerPage * 49;
-      setStart(newStart);
-      setEnd(newEnd);
-      fetchBanner();
-    }
-  }, [player]);
-
+const Player = ({ iframeUrl,title,image,page,MapedData,ID}) => {
   return (
     <>
       <Head>
-        <title>{`Player - ${video.Title || 'Watching On Free Netflix'}`}</title>
-        <meta name="description" content={video.Title || ''} />
+        <title>{`Player - ${title || 'Watching On Free Netflix'}`}</title>
+        <meta name="description" content={title || ''} />
       </Head>
-      {video && image && iframe ? (
-        <>
-          <div id="div1" className="aspect-video rounded-lg">
-            <iframe
-              frameborder="0"
-              allowFullScreen="1"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              title=""
-              width="100%"
-              height="100%"
-              src={iframe}
-            ></iframe>
-          </div>
-          <h1 className="m-5 text-xl max-w-lg">{video.Title}</h1>
-          <Link href="https://beta.publishers.adsterra.com/referral/aKMU588PJU">
-            <Image alt="banner" className="rounded-lg w-80 m-5" src={image} width={1000} height={1000} />
-          </Link>
-        </>
-      ) : (
-        <div id="div1" className="aspect-video rounded-lg"></div>
-      )}
+      <div id="div1" className="aspect-video rounded-lg">
+        <iframe
+          frameborder="0"
+          allowFullScreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          title=""
+          width="100%"
+          height="100%"
+          src={iframeUrl}
+        ></iframe>
+      </div>
+      <h1 className="m-5 text-xl max-w-lg">{title}</h1>
+      <Link href="https://beta.publishers.adsterra.com/referral/aKMU588PJU">
+        <Image alt="banner" className="rounded-lg w-80 m-5" src={image} width={1000} height={1000} />
+      </Link>
       <Link href="https://beta.publishers.adsterra.com/referral/aKMU588PJU">
         <Image
           alt="banner"
@@ -103,38 +35,62 @@ export default function Player() {
         />
       </Link>
       <div id="container-0ad2289ea9e1e73bd2b5439c23a4fc36"></div>
-      {pagedata.length !== 0 ? <ViewAllRender data={pagedata} /> : <EmptyViewAllRender />}
-      {pagedata.length !== 0 && (
-        <div className="flex mb-20 w-full justify-center items-center">
-          <button
-            className="bg-red-700 p-2 rounded-md m-3"
-            style={{ width: '100px' }}
-            disabled={+player[1] === 1}
-            onClick={() => {
-              if (+player[1] > 1) {
-                router.push(`/player/${player[0]}/${+player[1] - 1}`);
-              }
-            }}
-          >
-            {+player[1] === 1 ? 'First Page' : 'Back'}
-          </button>
-          <button className="bg-red-700 p-2 rounded-md m-3" style={{ width: '60px' }} disabled>
-            {+player[1]}
-          </button>
-          <button
-            className="bg-red-700 p-2 rounded-md m-3"
-            style={{ width: '100px' }}
-            disabled={pagedata.length < 50}
-            onClick={() => {
-              router.push(`/player/${player[0]}/${+player[1] + 1}`);
-            }}
-          >
-            {pagedata.length < 50 ? 'Last Page' : 'Next'}
-          </button>
-        </div>
-      )}
+      <div className='grid grid-cols-5 gap-3 mb-5'>
+        {MapedData.map((item)=>{
+            return <div key={item.ID} className='aspect-video'>
+                <Link href={`/player/${item.ID}/1`}><Image className='rounded h-full w-full cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-125 hover:bg-indigo-500 duration-300' src={item.Image} alt={item.Title} title={item.Title} width={500} height={500} /></Link>
+            </div>
+        })}
+      </div>
+      <div className="flex mb-20 w-full justify-center items-center">
+        <Link className="bg-red-700 p-2 rounded-md m-3" style={{ width: '100px' }} href={page<=1?"#":`/player/${ID}/${page-1}`}>{page === 1 ? 'First Page' : 'Back'}</Link>
+        <button className="bg-red-700 p-2 rounded-md m-3" style={{ width: '60px' }} disabled>{page}</button>
+        <Link disabled={MapedData.length < 50} href={`/player/${ID}/${page+1}`}><button className="bg-red-700 p-2 rounded-md m-3" style={{ width: '100px' }} disabled={MapedData.length < 50} >{MapedData.length < 50 ? 'Last Page' : 'Next'}</button></Link>
+      </div>
       <Script async data-cfasync="false" src="//toothbrushlimbperformance.com/0ad2289ea9e1e73bd2b5439c23a4fc36/invoke.js"></Script>
       <Script type='text/javascript' src='//toothbrushlimbperformance.com/e3/84/8c/e3848cc2ba52f8c8c8a0c97335996bea.js'></Script>
     </>
   );
+};
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  let page = +params.player[1];
+  if(page<=0){
+    page = 1;
+  }
+  const Start = (page - 1)*49;
+  const End = page*49;
+  const supabase =  createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_KEY);
+  const response = (await supabase.from('Free-Netflix-Darabase').select('*').eq('ID', params.player[0])).data[0];
+  const title = response.Title;
+  const image = response.Image;
+  const fileid = response.FileID;
+  const ID = response.ID;
+  let iframeUrl = '';
+  if (response.Plateform === 'filemoon') {
+    iframeUrl = `https://filemoon.sx/e/${fileid}?poster=${image}`;
+  } else if (response.Plateform === 'Youtube') {
+    iframeUrl = `https://www.youtube.com/embed/${fileid}`;
+  } else if (response.Plateform === 'Vidsrc') {
+    const embedType = response.MainCategory === 'TV' ? 'tv' : 'movie';
+    iframeUrl = `https://vidsrc.to/embed/${embedType}/${fileid}`;
+  } else if (response.Plateform === 'vidsrc') {
+    const embedType = response.MainCategory === 'TV' ? 'tv' : 'movie';
+    iframeUrl = `https://vidsrc.me//embed//${embedType}?imdb=${fileid}`;
+  }
+  let MapedData = (await supabase.from('Free-Netflix-Darabase').select('*').order('ID', { ascending: false }).in('MainCategory', [response.MainCategory]).range(Start,End)).data;
+  return {
+    props: {
+      iframeUrl,
+      title,
+      image,
+      page,
+      MapedData,
+      fileid,
+      ID,
+    },
+  };
 }
+
+export default Player;
