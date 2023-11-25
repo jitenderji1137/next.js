d69e86e32241da11ef094e66e2d9892a9fa80c82
jitender <113350806+jitenderji1137@users.noreply.github.com>
2023-11-25 13:55:03 +0530

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Piedra&display=swap"/>
             <link rel="icon" href="/logo.png" type="image/png" sizes="32x32" />
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9040616366543275" crossorigin="anonymous"></script>
           </Head>
           <Navbar />
           <Component {...pageProps} />
